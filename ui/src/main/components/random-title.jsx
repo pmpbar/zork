34c4110557c5d08d77d15d243224be1196a4ac36
Taylor <taybart@gmail.com>
2018-03-08 14:25:26 -0700

    Moving to a component to be embedded

--- ui/src/main/components/random-title.jsx
+++ ui/src/main/components/random-title.jsx
@@ -20,6 +20,7 @@ export default class RandomTitle extends Component {
       ['num', 'people', 'Who', 'peopleDesc']
     ];
   }
+
   componentDidMount() {
     if (this.state.title === '') {
       this.generate()
--- ui/src/router.jsx
+++ ui/src/router.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Navbar from 'globals/containers/navbar';
-import Login from 'login/login';
 import App from 'main/app';
 
 const routes = (
   <div id="page-content-wrapper" className="full-page">
-    <Navbar />
     <div className="container-fluid full-page">
       <Route exact path="/" component={App} />
-      <Route exact path="/login" component={Login} />
     </div>
   </div>
 );
